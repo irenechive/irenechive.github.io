a50c49770e6ab0a71c23a5af69a3f7d61252eb4c
irenechive <irenechive@gmail.com>
2025-01-15 21:15:41 +0900

    .

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -6,9 +6,13 @@ function Footer() {
       <div className={styles.content}>
         <div className={styles['col--1']}>
           <div className={styles.divider} />
-          <p>queen of everything</p>
+          <a href="https://x.com/ovxbjh" target="_blank">
+            ⓒ {new Date().getFullYear()} <strong>ov.</strong> All rights reserved
+          </a>
         </div>
-        <div className={styles['col--2']}>IRENE</div>
+        <div className={styles['col--2']}>
+          for <strong>IRENE</strong>
+        </div>
       </div>
     </footer>
   );
